0ddbe7bb2d81443a81e7208d8eb0acb366eaeda0
Andres_Marulanda <afmsamsung1981@gmail.com>
2024-06-26 22:58:40 -0500

    Se inicia curso de introducción a JS con libro de texto Eloquent

--- js/MiPrimerScript.js
+++ js/MiPrimerScript.js
@@ -1,3 +1,36 @@
 const miPrimerScript = () => {
+    prompt("Ingrese su contraseña: ")
+    
     alert("Hola, bienvenido al mundo de Javascript");
 }
+
+// Declarar una variable.
+let diez = 10;
+console.log("Diez * Diez = " + diez * diez);
+
+let humor = "buen";
+console.log(humor);
+
+humor = "mal";
+console.log(humor);
+
+let deuda = 1400000;
+deuda -= 200000;
+console.log("saldo de la deuda: $" + deuda);
+
+let vacia;
+console.log("variable vacia = " + vacia);
+
+let numero1 = 10 , numero2 = 20;
+console.log("numero1 + numero2 = " + (numero1 + numero2));
+
+// Las palabras reservadas var y const.
+var saludo1 = "Hola";
+const saludo2 = "Andres Felipe";        // Constante.
+console.log(saludo1 + " " + saludo2);
+
+
+
+
+
+
