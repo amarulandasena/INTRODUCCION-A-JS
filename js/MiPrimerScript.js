a4d9b05eeca33d850263127ebef3470c03b507dc
Andres_Marulanda <afmsamsung1981@gmail.com>
2024-06-28 17:01:48 -0500

    se siguen trabajando los conceptos fundamentales, relacionados con las estructuras condicionales y los ciclos while y for.

--- js/MiPrimerScript.js
+++ js/MiPrimerScript.js
@@ -1,7 +1,42 @@
 const miPrimerScript = () => {
     prompt("Ingrese su contraseña: ")
-    
     alert("Hola, bienvenido al mundo de Javascript");
+
+    // Introducción a funciones.
+    let numero;
+    numero = Number(prompt("Ingrese un número entero: "));
+    let cuadrado;
+    cuadrado = Math.pow(numero, 2);
+    alert("El cuadrado del número ingresado es: " + cuadrado);
+
+    // Condicional simple.
+    let numero1 = Number(prompt("Ingrese un nuevo número: "));
+    if(!Number.isNaN(numero1)){
+        alert("El cuadrado del número ingresado es: " + Math.pow(numero1, 2));
+    };
+
+    // Condicional doble.
+    let numero2 = Number(prompt("Ingrese un número entero entre 0 y 20 : "));
+    if (numero2 <= 10){
+        alert("El número es menor o igual que 10");
+    } else {
+        alert("El número es mayor que 10.");
+    };
+
+    // Condicional multiple.
+    let numero3 = Number(prompt("Ingrese un número entre 0 y 100: "));
+    if (numero3 <= 10){
+        alert("El número es muy pequeño");
+    } else if (numero3 >= 10 && numero3 < 30){
+        alert("El número es pequeño");
+    } else if (numero3 >= 30 && numero3 < 60){
+        alert("El número es grande");
+    } else if(numero3 >= 60 && numero3 < 100){
+        alert("El número es muy grande");
+    } else {
+        alert("El número es mayor que 100");
+    }
+
 }
 
 // Declarar una variable.
@@ -28,6 +63,35 @@ console.log("numero1 + numero2 = " + (numero1 + numero2));
 var saludo1 = "Hola";
 const saludo2 = "Andres Felipe";        // Constante.
 console.log(saludo1 + " " + saludo2);
+
+// Introducción a funciones.
+console.log(Math.min(2,4,6,8,10,12,3,5,7,9,11));
+console.log(Math.max(3,5,7,9,11) + 101 + Math.min(12,10,8,6,4,2));
+console.log(Math.pow(3,2));
+
+// While.
+let contador = 0;
+let potencia = 1;       // Almacenará el resultado de cada multiplicación.
+
+while (contador < 10){
+    potencia *= 2;      // Calcular el valor  de 2 a la 10.
+    contador++;
+}
+console.log("2 ** 10 = " + potencia);
+
+// for.
+for (let contador1 = 0; contador1 <= 10; contador1 += 2){
+    console.log(contador1);
+}
+
+// Break.
+for (let contador2 = 2; contador2 <= 10; contador2 +=1){
+    console.log(contador2);
+    if ((20 / contador2) == 5){
+        break;
+    }
+}
+
 
 
 
