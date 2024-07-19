10e4e72d01f62922b549a54afc086e12fa5ec7cb
Andres_Marulanda <afmsamsung1981@gmail.com>
2024-07-19 12:35:28 -0500

    SE trabajan las funciones de orden superior que permiten filtrar, mapear y reducir los elementos de un arreglo.

--- js/FuncionesDeOrdenSuperior.js
+++ js/FuncionesDeOrdenSuperior.js
@@ -13,3 +13,49 @@ repetirAccion(6, i => {
     etiquetas.push(`Unidad ${i+1}`);
 });
 console.log(etiquetas);
+
+// Funciones de OS.
+let personas = [{
+    "id" : 1012123456,
+    "nombre" : "Juan carlos",
+    "apellido" : "Jimenez",
+    "mayor_de_edad" : true
+},{
+    "id" : 1025638697,
+    "nombre" : "Juliana",
+    "apeliido" : "Perez",
+    "mayor_de_edad" : true
+},{
+    "id" : 1024785698,
+    "nombre" : "Vicente",
+    "apellido" : "Nieto",
+    "mayor_de_edad" : false
+},{
+    "id" : 1458759896,
+    "nombre" : "Maria Camila",
+    "apellido" : "Gutierrez",
+    "mayor_de_edad" : true
+}];
+
+const filtrar = (array, prueba) => {
+    let cumplen = [];
+    for (let elemento of array){
+        if (prueba(elemento)){
+            cumplen.push(elemento);
+        }
+    }
+    return cumplen;
+}
+
+const mapear = (array, mapeo) => {
+    let mapeados = [];
+    for (let elemento of array){
+        mapeados.push(mapeo(elemento));
+    }
+    return mapeados;
+}
+let elementosFiltrados = filtrar(personas, personas => personas.mayor_de_edad);
+console.log(mapear(elementosFiltrados, personas => personas.nombre));
+
+
+
