9231fa6e6213eed338dfd7087a6c3a6f8e6a9516
Andres_Marulanda <afmsamsung1981@gmail.com>
2024-07-26 13:41:24 -0500

    Se continua trabajando la notación de clases.

--- js/IntroduccionAObjetos.js
+++ js/IntroduccionAObjetos.js
@@ -25,3 +25,20 @@ conejoHambriento.hablar1("podría comerme una zanahoria ahora mismo.");
 
 hablar1.call(conejoBlanco, "oh my god!");       // puedo usar el método call para usar funciones como métodos de objetos.
 
+// Mapas.
+let edades = new Map();
+edades.set("Boris", 39);
+edades.set("Liang", 22);
+edades.set("Julia", 62);
+
+/* console.log(`Claves del mapa: ${edades.keys()}`); */
+console.log(`Julia tiene ${edades.get("Julia")}`);
+console.log(edades.has("Felipe"));
+/* console.log(edades.hasOwnProperty("Liang")); */
+
+// Iteradores.
+let iteradorOK = "OK"[Symbol.iterator]();
+console.log(iteradorOK.next());             /* value = valor del caracter, done = que me indica si hay o no mas resultados */
+console.log(iteradorOK.next());
+console.log(iteradorOK.next());
+
---
+++ js/NotacionDeClase.js
@@ -0,0 +1,83 @@
+// Notación de clase.
+class Vehiculo {
+    constructor (marca, referencia, modelo, valor) {
+        this.marca = marca;
+        this.referencia = referencia;
+        this.modelo = modelo;
+        this.valor = valor;
+    }
+
+    mostrarEnPantalla () {
+        console.log (`        
+        Marca: ${this.marca} 
+        Referencia: ${this.referencia}
+        Modelo:  ${this.modelo}
+        Valor: ${this.valor}`
+        );
+    }
+
+    calcularDescuento () {
+        return this.valor - this.valor * 0.1;
+    }
+}
+const vehiculo1 = new Vehiculo ("Renault", "Megane", 2020, 42000000);
+vehiculo1.mostrarEnPantalla();
+console.log(vehiculo1.calcularDescuento());
+
+// Array bidimensional.
+class Matriz {
+    constructor (ancho, altura, elemento = (x,y) => undefined) {
+        this.ancho = ancho;
+        this.altura = altura;
+        this.contenido = [];
+
+        // Como se le dan valores al atributo contenido.
+        for (let y = 0; y < altura; y++){
+            for (let x = 0; x < ancho; x++){
+                this.contenido[y * ancho + x] = elemento(x,y);
+            }
+        }
+    }
+
+    obtener () {
+        return this.contenido[y * this.ancho + x];
+    }
+
+    establecer (x, y, valor) {
+        this.contenido[y * this.ancho + x] = valor;
+    }
+}
+
+class IteradorMatriz {
+    constructor (matriz) {
+        this.x = 0;
+        this.y = 0;
+        this.matriz = matriz;
+    }
+
+    next () {
+        if (this.y == this.matriz.altura) return {done : true};
+
+        let value = {
+            x : this.x,
+            y : this.y,
+            value : this.matriz.obtener(this.x, this.y)
+        }
+        this.x++
+        if (this.x == this.matriz.ancho) {
+            this.x = 0;
+            this.y++;
+        }
+        return {value, done : false};
+    }
+}
+
+let matriz = new Matriz (2, 2, (x,y) => `valor ${x},${y}`);
+for (let {x, y, value} of matriz){
+    console.log(x, y, value);
+}
+
+
+
+
+
