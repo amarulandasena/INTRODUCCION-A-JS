28472156cacfc65e7a47f5cce413734e9bcc6ed4
Andres_Marulanda <afmsamsung1981@gmail.com>
2024-09-05 20:46:13 -0500

    Se trabaja un ejemplo de manejo de excepciones

--- js/BugsYErrores.js
+++ js/BugsYErrores.js
@@ -11,3 +11,32 @@ Probar("convertir texto griego a mayúsculas", () => {
     return "Χαίρετε".toUpperCase == "XAIPETE";
 });
 
+// Manejo de excepciones
+const pedirDireccion = (pregunta) => {
+    let resultado = prompt(pregunta);
+
+    if (resultado.toLowerCase == "derecha") {
+        return "D";
+    }
+
+    if (resultado.toLowerCase == "izquierda") {
+        return "I";
+    }
+
+    throw new Error("Dirección invalida: " + resultado);
+}
+
+const mirar = () => {
+    if (pedirDireccion("Hacia qué diracción quieres ir?") == "I") {
+        return "una casa";
+    } else {
+        return "dos osos furiosos";
+    }
+}
+
+try {
+    console.log("Tú ves " + mirar());
+} catch (error) {
+    console.log ("Algo incorrecto sucedió: " + error);
+}
+
