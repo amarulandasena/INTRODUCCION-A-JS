8424dd76b93e551473c7c4722a035cf5f10a574f
Andres_Marulanda <afmsamsung1981@gmail.com>
2024-09-06 22:51:26 -0500

    Se termina el caítulo de Errores

--- js/BugsYErrores.js
+++ js/BugsYErrores.js
@@ -11,7 +11,11 @@ Probar("convertir texto griego a mayúsculas", () => {
     return "Χαίρετε".toUpperCase == "XAIPETE";
 });
 
-// Manejo de excepciones
+/* Manejo de excepciones
+class ErrorDeEntrada extends Error {
+
+} */
+
 const pedirDireccion = (pregunta) => {
     let resultado = prompt(pregunta);
 
@@ -27,7 +31,7 @@ const pedirDireccion = (pregunta) => {
 }
 
 const mirar = () => {
-    if (pedirDireccion("Hacia qué diracción quieres ir?") == "I") {
+    if (pedirDireccion("Hacia qué dirección quieres ir?") == "I") {
         return "una casa";
     } else {
         return "dos osos furiosos";
@@ -40,3 +44,37 @@ try {
     console.log ("Algo incorrecto sucedió: " + error);
 }
 
+// Sistma bancario
+const cuentas = {
+    a : 100,
+    b : 20,
+    c : 0
+}
+
+let obtenerCuenta = () => {
+    let nombreCuenta = prompt("Ingrese el nombre de la cuenta: ");
+    
+    if (!cuentas.hasOwnProperty(nombreCuenta)) {
+        throw new Error (`La cuenta "${nombreCuenta}" no existe`);
+    }
+
+    return nombreCuenta;
+}
+
+let transferencia = (desde, cantidad) => {
+    if (cuentas[desde] < cantidad) {
+        return ;
+    }
+
+    let progreso = 0;
+    try {
+        cuentas[desde] -= cantidad;
+        progreso = 1;
+        cuentas[obtenerCuenta()] += cantidad;
+        progreso = 2;
+    } finally {
+        if (progreso == 1) {
+            cuentas[desde] += cantidad;
+        }
+    }
+}
