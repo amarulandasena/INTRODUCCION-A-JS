9150f46c4012cc8d8a06a123fc3ce3f8a8eb0da7
Andres_Marulanda <afmsamsung1981@gmail.com>
2024-09-12 19:02:54 -0500

    Ejemplo manejo de excepciones con promesas

--- js/Asincronicas.js
+++ js/Asincronicas.js
@@ -11,13 +11,13 @@ granRoble.leerAlmacenamiento ("caches de alimentos", caches => {
 // Enviar incormación entre los nidos.
 granRoble.send ("Pastura de vacas", "nota", "Vamos a graznar a las 7pm", () => console.log("Nota recibida."));
 
-// REcibir la solicitud y manejarla.
+// Recibir la solicitud y manejarla.
 import {definirTipoSolicitud} from "./tecnologia-cuervo";
 
-definirTipoSolicitud("nota", (nido, contenido, fuente, listo) => {
+/*definirTipoSolicitud("nota", (nido, contenido, fuente, listo) => {
     console.log (`${nido.nombre} recibió nota: ${contenido}`);
     listo();
-})
+})*/
 
 // Utilizando promesas.
 function almacenamiento(nido, nombre) {
@@ -27,3 +27,42 @@ function almacenamiento(nido, nombre) {
 }
 
 almacenamiento(granRoble, "enemigos").then(valor => console.log("obtuve", valor));
+
+// Manejo de excepciones con promesas.
+function request (nido, objetivo, tipo, contenido) {
+    
+    return new Promise ((resolve, reject) => {
+        let listo = false;
+        function intentar (n) {
+            nido.send(objetivo, tipo, contenido, (fallo, value) => {
+                listo = true;
+                if (fallo) {
+                    reject (fallo);
+                } else {
+                    resolve (value);
+                }
+            });
+
+            setTimeout (() => {
+                if (listo) {
+                    return;
+                } else if (n < 3) {
+                    intentar (n + 1);
+                } else {
+                    reject (new TiempoDeEspera ("Tiempo de espera agotado"));
+                }
+            }, 250);
+        }
+        intentar (1);
+    });
+}
+
+function tipoDeSolicitud (nombre, manejador) {
+    definirTipoSolicitud (nombre, (nido, contenido, fuente, devolucionDeLLamada) => {
+        try {
+            Promise.resolve (manejador(nido, contenido, fuente)).then(response => devolucionDeLLamada(null, response), failure => devolucionDeLLamada(failure));
+        } catch (exception) {
+            devolucionDeLLamada(exception);
+        }
+    });
+}
